a5746a52dd7f629be543cd2964c8d919fda77e2a
drjjjm18 <drjjm18@gmail.com>
2022-09-15 22:02:41 +0100

    use argsparse to pass episode count to main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import environment
 import snake_agent
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
+import argparse
 
 from train import train_driver
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--episodes', default=100)
+args = parser.parse_args()
+episodes = int(args.episodes)
+
 
 env = environment.SnakeEnvironment()
 env = TFPyEnvironment(env)
@@ -12,7 +19,9 @@ agent.initialize()
 
 driver = train_driver(env, agent)
 
-results, losses = driver.run(1)
+
+print(f'playing and training for {episodes} episodes')
+results, losses = driver.run(episodes)
 print(f"""
 first result = {results[0]}
 last result = {results[-1]}
